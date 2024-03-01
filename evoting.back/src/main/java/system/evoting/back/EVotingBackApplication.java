d38f05cc2de0d2fa79a3dcd077423a77ba1df951
momosylla <mamadou.sylla@atos.net>
2024-03-01 22:55:29 +0000

    Initialization of back-end & architecture of e-voting project

---
+++ evoting.back/src/main/java/system/evoting/back/EVotingBackApplication.java
@@ -0,0 +1,13 @@
+package system.evoting.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVotingBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EVotingBackApplication.class, args);
+	}
+
+}
